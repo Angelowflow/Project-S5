a71e3edeaa5c40a9f17cb574944af6d43b605e4c
Tecno <163066910+Angelowflow@users.noreplay.github.com>
2024-03-30 14:41:07 -0600

    Second versión project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Project-S5
 Repositorio para aplicación web
+Esta aplicación web sirve para conocer información relacionada al kilometraje de autos en venta, mediante el uso de  gráficos
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+st.header('Graficar información de venta de autos')
 car_data = pd.read_csv('vehicles_us.csv')
 
 hist_button = st.button('Construir histograma') #crear botón histograma
@@ -11,8 +12,9 @@ if hist_button:
     fig = px.histogram(car_data, x="odometer") # crear un histograma
     st.plotly_chart(fig, use_container_width=True) # mostrar un gráfico Plotly interactivo
 
+scatter_button = st.button('Construir gráfico de dispersión') #crear botón dispersión
+
 if scatter_button:
     st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
     fig = px.scatter(car_data, x="odometer", y='price') # crear un gráfico de dispersión
     st.plotly_chart(fig, use_container_width=True) # mostrar un gráfico Plotly interactivo
-
